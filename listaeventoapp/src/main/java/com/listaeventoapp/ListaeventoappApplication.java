04abcc44a64ab47aa66a626f11edcf0c35468c82
victorcav <victor.bcav@gmail.com>
2021-10-05 15:27:53 -0300

    Iniciando aplicação de Eventos com o Spring Boot

---
+++ listaeventoapp/src/main/java/com/listaeventoapp/ListaeventoappApplication.java
@@ -0,0 +1,13 @@
+package com.listaeventoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListaeventoappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListaeventoappApplication.class, args);
+	}
+
+}
---
+++ listaeventoapp/src/test/java/com/listaeventoapp/ListaeventoappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.listaeventoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListaeventoappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
